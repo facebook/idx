b7ea5a649ee54ea50e61db2acd26ac567761030e
Andres Suarez <zertosh@gmail.com>
2017-08-29 20:38:02 -0400

    Remove fast "idx" binding check

--- packages/babel-plugin-idx/src/babel-plugin-idx.js
+++ packages/babel-plugin-idx/src/babel-plugin-idx.js
@@ -224,7 +224,7 @@ module.exports = context => {
     visitor: {
       Program(path, state) {
         // If there can't reasonably be an idx call, exit fast.
-        if (path.scope.getOwnBinding('idx') || idxRe.test(state.file.code)) {
+        if (idxRe.test(state.file.code)) {
           // We're very strict about the shape of idx. Some transforms, like
           // "babel-plugin-transform-async-to-generator", will convert arrow
           // functions inside async functions into regular functions. So we do
