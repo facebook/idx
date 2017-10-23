00fabe5f28f84d10e9603acd435762de81bd9336
phra <greensoncio@gmail.com>
2017-10-23 14:54:23 +0200

    fix: typo in typescript definitions

--- packages/idx/src/idx.d.ts
+++ packages/idx/src/idx.d.ts
@@ -3,5 +3,5 @@
  * @param { Function } accessor - Accessor function
  * @return { T2 }
  */
-export declare function idx<T1, T2>(prop: T1, accessor: (T1) => T2): T2 | null | undefined;
+export declare function idx<T1, T2>(prop: T1, accessor: (prop: T1) => T2): T2 | null | undefined;
 export default idx;
