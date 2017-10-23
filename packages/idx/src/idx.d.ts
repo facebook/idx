d697ce17bac5fceeedd4b4e9798fee2c997c4620
phra <greensoncio@gmail.com>
2017-10-23 12:06:09 +0200

    feat: add typescript definitions

---
+++ packages/idx/src/idx.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @param { T1 } prop - Parent Object
+ * @param { Function } accessor - Accessor function
+ * @return { T2 }
+ */
+export declare function idx<T1, T2>(prop: T1, accessor: (T1) => T2): T2 | null | undefined;
+export default idx;
