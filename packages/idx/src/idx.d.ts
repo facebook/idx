c2450dc0c74bc718ba418d943daabc931fbd5a4b
Malash <i@malash.me>
2018-11-05 11:12:46 +0800

    remove useless export for TS

--- packages/idx/src/idx.d.ts
+++ packages/idx/src/idx.d.ts
@@ -3,5 +3,8 @@
  * @param { Function } accessor - Accessor function
  * @return { T2 }
  */
-export declare function idx<T1, T2>(prop: T1, accessor: (prop: T1) => T2): T2 | null | undefined;
+declare function idx<T1, T2>(
+  prop: T1,
+  accessor: (prop: T1) => T2,
+): T2 | null | undefined;
 export default idx;
