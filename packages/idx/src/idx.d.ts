f948e3e4dd62bb4b9180b381c552e4bc883b335f
Mohsen Azimi <mohsen.azimi@airbnb.com>
2018-11-26 16:42:48 -0800

    Allow nullables to be passed as a prop in TypeScript
    Also simplify FunctionWithRequiredReturnType and use the built in
    NonNullable

--- packages/idx/src/idx.d.ts
+++ packages/idx/src/idx.d.ts
@@ -1,22 +1,15 @@
 /**
- * NonUndefinedOrnNull
- * Exclude undefined and null from set `A`
- */
-type NonUndefinedOrnNull<T> = T extends (undefined | null) ? never : T;
-
-/**
  * DeepRequiredArray
  * Nested array condition handler
  */
-interface DeepRequiredArray<T>
-  extends Array<DeepRequired<NonUndefinedOrnNull<T>>> {}
+interface DeepRequiredArray<T> extends Array<DeepRequired<NonNullable<T>>> {}
 
 /**
  * DeepRequiredObject
  * Nested object condition handler
  */
 type DeepRequiredObject<T> = {
-  [P in keyof T]-?: DeepRequired<NonUndefinedOrnNull<T[P]>>
+  [P in keyof T]-?: DeepRequired<NonNullable<T[P]>>
 };
 
 /**
@@ -24,131 +17,8 @@ type DeepRequiredObject<T> = {
  */
 type FunctionWithRequiredReturnType<
   T extends (...args: any[]) => any
-> = T extends FunctionWithRequiredReturnType_RestArgs<T>
-  ? FunctionWithRequiredReturnType_RestArgs<T>
-  : T extends FunctionWithRequiredReturnType_Args1<T>
-    ? FunctionWithRequiredReturnType_Args1<T>
-    : T extends FunctionWithRequiredReturnType_Args2<T>
-      ? FunctionWithRequiredReturnType_Args2<T>
-      : T extends FunctionWithRequiredReturnType_Args3<T>
-        ? FunctionWithRequiredReturnType_Args3<T>
-        : T extends FunctionWithRequiredReturnType_Args4<T>
-          ? FunctionWithRequiredReturnType_Args4<T>
-          : T extends FunctionWithRequiredReturnType_Args5<T>
-            ? FunctionWithRequiredReturnType_Args5<T>
-            : T extends FunctionWithRequiredReturnType_Args6<T>
-              ? FunctionWithRequiredReturnType_Args6<T>
-              : T extends FunctionWithRequiredReturnType_Args7<T>
-                ? FunctionWithRequiredReturnType_Args7<T>
-                : T extends FunctionWithRequiredReturnType_NoArgs<T>
-                  ? FunctionWithRequiredReturnType_NoArgs<T>
-                  : FunctionWithRequiredReturnType_AnyArgs<T>;
-
-/**
- * Function that has deeply required return type with no arguments
- */
-type FunctionWithRequiredReturnType_AnyArgs<
-  T extends (...args: any[]) => any
-> = T extends (...args: any[]) => infer R
-  ? (...args: any[]) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with rest argument
- */
-type FunctionWithRequiredReturnType_RestArgs<
-  T extends (...args: any[]) => any
 > = T extends (...args: infer A) => infer R
   ? (...args: A) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with no arguments
- */
-type FunctionWithRequiredReturnType_NoArgs<
-  T extends () => any
-> = T extends () => infer R ? () => DeepRequired<R> : never;
-
-/**
- * Function that has deeply required return type with 1 argument
- */
-type FunctionWithRequiredReturnType_Args1<
-  T extends (arg: any) => any
-> = T extends (a: infer A) => infer R ? (a: A) => DeepRequired<R> : never;
-
-/**
- * Function that has deeply required return type with 2 arguments
- */
-type FunctionWithRequiredReturnType_Args2<
-  T extends (arg: any) => any
-> = T extends (a: infer A, b: infer B) => infer R
-  ? (a: A, b: B) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with 3 arguments
- */
-type FunctionWithRequiredReturnType_Args3<
-  T extends (arg: any) => any
-> = T extends (a: infer A, b: infer B, c: infer C) => infer R
-  ? (a: A, b: B, c: C) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with 4 arguments
- */
-type FunctionWithRequiredReturnType_Args4<
-  T extends (arg: any) => any
-> = T extends (a: infer A, b: infer B, c: infer C, d: infer D) => infer R
-  ? (a: A, b: B, c: C, d: D) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with 5 arguments
- */
-type FunctionWithRequiredReturnType_Args5<
-  T extends (arg: any) => any
-> = T extends (
-  a: infer A,
-  b: infer B,
-  c: infer C,
-  d: infer D,
-  e: infer E,
-) => infer R
-  ? (a: A, b: B, c: C, d: D, e: E) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with 6 arguments
- */
-type FunctionWithRequiredReturnType_Args6<
-  T extends (arg: any) => any
-> = T extends (
-  a: infer A,
-  b: infer B,
-  c: infer C,
-  d: infer D,
-  e: infer E,
-  f: infer F,
-) => infer R
-  ? (a: A, b: B, c: C, d: D, e: E, f: F) => DeepRequired<R>
-  : never;
-
-/**
- * Function that has deeply required return type with 7 arguments
- */
-type FunctionWithRequiredReturnType_Args7<
-  T extends (arg: any) => any
-> = T extends (
-  a: infer A,
-  b: infer B,
-  c: infer C,
-  d: infer D,
-  e: infer E,
-  f: infer F,
-  g: infer G,
-) => infer R
-  ? (a: A, b: B, c: C, d: D, e: E, f: F, g: G) => DeepRequired<R>
   : never;
 
 /**
@@ -196,6 +66,6 @@ type DeepRequired<T> = T extends any[]
  */
 declare function idx<T1, T2>(
   prop: T1,
-  accessor: (prop: DeepRequired<T1>) => T2,
+  accessor: (prop: NonNullable<DeepRequired<T1>>) => T2,
 ): T2 | null | undefined;
 export default idx;
