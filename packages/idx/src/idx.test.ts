c0ae9530784414b57db84f440fd9cbc7e677dff8
Mohsen Azimi <mohsen.azimi@airbnb.com>
2018-11-26 16:42:48 -0800

    Allow nullables to be passed as a prop in TypeScript
    Also simplify FunctionWithRequiredReturnType and use the built in
    NonNullable

--- packages/idx/src/idx.test.ts
+++ packages/idx/src/idx.test.ts
@@ -21,6 +21,9 @@ let item: string | undefined | null = idx(
   _ => _.foo.bar.baz.arr[0].inner.item,
 );
 
+let baz = idx(deep, _ => _.foo.bar.baz);
+item = idx(baz, _ => _.arr[0].inner.item);
+
 let listOfDeep: DeepStructure[] = [];
 
 item = idx(listOfDeep, _ => _[0].foo.bar.baz.arr[0].inner.item);
@@ -35,7 +38,7 @@ interface NullableStructure {
 
 let nullable: NullableStructure = {} as any;
 
-let baz: string | null | undefined = idx(nullable, _ => _.foo.bar.baz);
+let bazz: string | null | undefined = idx(nullable, _ => _.foo.bar.baz);
 
 interface WithMethods<T = any> {
   foo?: {
@@ -45,7 +48,19 @@ interface WithMethods<T = any> {
     fn?(): {inner?: string};
   };
   args?(a: number): number;
-  manyArgs?(a: number, b: string, c: boolean): number;
+  manyArgs?(
+    a: number,
+    b: string,
+    c: boolean,
+    d: number,
+    e: number,
+    f: number,
+    g: string,
+    h: string,
+    k: number,
+    l: boolean,
+    m: string,
+  ): number;
   restArgs?(...args: string[]): string;
   genrric?(arg: T): T;
 }
@@ -54,7 +69,9 @@ let withMethods: WithMethods<boolean> = {} as any;
 
 let n: number | undefined | null = idx(withMethods, _ => _.foo.bar());
 n = idx(withMethods, _ => _.args(1));
-n = idx(withMethods, _ => _.manyArgs(1, 'b', true));
+n = idx(withMethods, _ =>
+  _.manyArgs(1, 'b', true, 1, 2, 3, '4', '5', 1, true, ''),
+);
 let s: string | undefined | null = idx(withMethods, _ => _.baz.fn().inner);
 s = idx(withMethods, _ => _.restArgs('1', '2', '3', '4', '5', '6'));
 let b: boolean | undefined | null = idx(withMethods, _ => _.genrric(true));
